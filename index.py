b10e189956a1a34215cb79c6d9ba05d001168412
Carver Morrison <12cmorrison12@gmail.com>
2023-11-30 09:15:33 -0700

    added in code to fetch random index from api

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, render_template
+import random
 
 
 app = Flask(__name__)
 
-url = "https://www.dnd5eapi.co/api/magic-items/:index"
+API_URL = "https://www.dnd5eapi.co/api/magic-items/:index"
 
 
 payload = {}
@@ -12,20 +13,38 @@ headers = {
 }
 
 
-response = request.request("GET", url, headers=headers, data=payload)
+response = request.request("GET", API_URL, headers=headers, data=payload)
 
 
 print(response.text)
 
-@app.route('/')
-def index(name=None):
-    return render_template('index.html', name=name)
-
-# function to get random item (magic) from api, dropdown menu on front end
-@app.route('/items', methods=["GET", "POST"])
-def items(indexNum):
-    return 
-
 # function to get item rarity from dropdown
 
 # retrieve all data from the API and pick a random index num and return item with name, desc, rarity, url?
+
+def get_api_data():
+    response = request.get(API_URL)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+@app.route('/')
+def index():
+    api_data = get_api_data()
+
+    if api_data:
+        # Extract the ids from the API data
+        ids = [post['id'] for post in api_data]
+
+        # Generate a random index
+        random_index = random.choice(ids)
+
+        # Render the HTML template with the random index
+        return render_template('index_with_api.html', random_index=random_index)
+
+    else:
+        return "Error fetching data from the API"
+
+if __name__ == '__main__':
+    app.run(debug=True)
