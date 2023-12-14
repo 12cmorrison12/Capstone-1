2fde37f315567ad3e3bac53c90c0dc81785cea83
Carver Morrison <12cmorrison12@gmail.com>
2023-12-14 04:44:54 -0700

    added template for random item

--- index.py
+++ index.py
@@ -1,50 +1,27 @@
-from flask import Flask, request, render_template
-import random
-
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
-API_URL = "https://www.dnd5eapi.co/api/magic-items/:index"
+# D&D 5e API endpoint for items
+API_ENDPOINT = "https://www.dnd5eapi.co/api/equipment/"
 
+def get_random_item():
+    # Get a random item from the D&D 5e API
+    response = requests.get(API_ENDPOINT)
+    data = response.json()
+    return data
 
-payload = {}
-headers = {
-  'Accept': 'application/json'
-}
-
-
-response = request.request("GET", API_URL, headers=headers, data=payload)
-
-
-print(response.text)
-
-# function to get item rarity from dropdown
-
-# retrieve all data from the API and pick a random index num and return item with name, desc, rarity, url?
-
-def get_api_data():
-    response = request.get(API_URL)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
-
-@app.route('/')
+@app.route("/")
 def index():
-    api_data = get_api_data()
+    # Render the home page
+    return render_template("index.html")
 
-    if api_data:
-        # Extract the ids from the API data
-        ids = [post['id'] for post in api_data]
+@app.route("/random_item")
+def random_item():
+    # Get a random item and render the template with item details
+    item = get_random_item()
+    return render_template("random_item.html", item=item)
 
-        # Generate a random index
-        random_index = random.choice(ids)
-
-        # Render the HTML template with the random index
-        return render_template('index_with_api.html', random_index=random_index)
-
-    else:
-        return "Error fetching data from the API"
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
